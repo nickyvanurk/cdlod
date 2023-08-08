e6bf8624f53cf3dda215baec15b7c991acf25df0
Nicky van Urk <nickyvanurk@gmx.com>
2023-08-08 22:41:56 +0200

    Encapsulate three.js code

--- src/main.ts
+++ src/main.ts
@@ -1,34 +1,5 @@
 import './style.css'
-import * as THREE from 'three';
+import { Scene } from './scene';
 
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(71, window.innerWidth / window.innerHeight, 0.1, 1000);
-
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
-
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
-
-camera.position.z = 5;
-
-function render() {
-	requestAnimationFrame(render);
-
-	cube.rotation.x += 0.01;
-	cube.rotation.y += 0.01;
-
-	renderer.render(scene, camera);
-}
-
-function onWindowResize() {
-  camera.aspect = window.innerWidth / window.innerHeight;
-  camera.updateProjectionMatrix();
-  renderer.setSize(window.innerWidth, window.innerHeight);
-}
-
-window.addEventListener("resize", onWindowResize, false);
-render();
+const main = new Scene();
+main.render();
