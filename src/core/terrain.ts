5df971f9915790e56a9377f92e3b5e6212564e85
Nicky van Urk <nickyvanurk@gmx.com>
2023-09-16 20:33:57 +0200

    Calculate LOD ranges

--- src/core/terrain.ts
+++ src/core/terrain.ts
@@ -6,6 +6,14 @@ export class Terrain {
   constructor() {
     this.tree = new QuadTree();
 
+    const minLodDistance = 15;
+    const lodLevels = 6;
+    const lodRanges = [];
+
+    for (let i = 0; i < lodLevels; i++) {
+      lodRanges[i] = minLodDistance * Math.pow(2, lodLevels - 1 - i);
+    }
+
     console.log('New terrain');
   }
 }
