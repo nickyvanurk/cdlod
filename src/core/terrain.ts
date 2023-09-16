9fdf118d384bf5b2b5771e03a1a8ba4adb397de7
Nicky van Urk <nickyvanurk@gmx.com>
2023-09-16 21:44:08 +0200

    Load heightmap

--- src/core/terrain.ts
+++ src/core/terrain.ts
@@ -1,3 +1,4 @@
+import * as THREE from 'three';
 import { Group, Mesh, MeshBasicMaterial, PlaneGeometry } from 'three';
 
 import { QuadTree } from './quad_tree';
@@ -7,7 +8,7 @@ export class Terrain extends Group {
 
   constructor() {
     super();
-    this.rotateX(Math.PI / 2);
+    this.rotateX(-Math.PI / 2);
 
     this.tree = new QuadTree();
 
@@ -23,6 +24,15 @@ export class Terrain extends Group {
     const material = new MeshBasicMaterial({ color: 0xffffff, wireframe: true });
     const plane = new Mesh(geometry, material);
     this.add(plane);
+
+    const heightmapUrl =
+      'https://service.pdok.nl/rws/ahn/wms/v1_0?SERVICE=WMS&request=GetMap&version=1.3.0&Layers=dsm_05m&styles=default&crs=EPSG:28992&bbox=-285401.92,22598.08,595401.92,903401.92&width=129&height=129&format=image/jpeg';
+    const textureLoader = new THREE.TextureLoader();
+    textureLoader.crossOrigin = 'Anonymous';
+    const tex = textureLoader.load(heightmapUrl, () => {
+      plane.material.map = tex;
+      plane.material.needsUpdate = true;
+    });
 
     console.log('New terrain');
   }
