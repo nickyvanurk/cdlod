a4cb040123ad06229791c03be4ec7c3ddbe5781d
Nicky van Urk <nickyvanurk@gmx.com>
2023-09-19 21:40:28 +0200

    Add terrain again

--- src/core/scene.ts
+++ src/core/scene.ts
@@ -1,26 +1,33 @@
 import * as THREE from 'three';
 
-import gridVertexShader from './grid.vs';
+// import gridVertexShader from './grid.vs';
+import { Terrain } from '@core/terrain';
 
 export class Scene extends THREE.Scene {
-  constructor(_camera: THREE.PerspectiveCamera) {
+  private terrain = new Terrain();
+
+  constructor(private camera: THREE.PerspectiveCamera) {
     super();
 
-    const sectorSize = 64;
-    const geometry = new THREE.PlaneGeometry(2048, 2048, sectorSize * 2, sectorSize * 2);
-    geometry.rotateX(-Math.PI / 2); // flip to xz plane
+    this.add(this.terrain);
 
-    const material = new THREE.ShaderMaterial({
-      uniforms: {
-        sectorSize: { value: sectorSize },
-      },
-      vertexShader: gridVertexShader,
-      wireframe: true,
-    });
+    // const sectorSize = 64;
+    // const geometry = new THREE.PlaneGeometry(2048, 2048, sectorSize * 2, sectorSize * 2);
+    // geometry.rotateX(-Math.PI / 2); // flip to xz plane
 
-    const grid = new THREE.InstancedMesh(geometry, material, 1);
-    this.add(grid);
+    // const material = new THREE.ShaderMaterial({
+    //   uniforms: {
+    //     sectorSize: { value: sectorSize },
+    //   },
+    //   vertexShader: gridVertexShader,
+    //   wireframe: true,
+    // });
+
+    // const grid = new THREE.InstancedMesh(geometry, material, 1);
+    // this.add(grid);
   }
 
-  update(_dt: number) {}
+  update(_dt: number) {
+    this.terrain.update(this.camera.position);
+  }
 }
