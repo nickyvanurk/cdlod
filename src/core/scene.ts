69e9ccb0682a8aceccce413cb78c7ea443eb7ed2
Nicky van Urk <nickyvanurk@gmx.com>
2023-09-19 20:43:34 +0200

    Remove terrain for now

--- src/core/scene.ts
+++ src/core/scene.ts
@@ -1,18 +1,9 @@
 import * as THREE from 'three';
 
-import { Terrain } from './terrain';
-
 export class Scene extends THREE.Scene {
-  private terrain: Terrain;
-
   constructor(private camera: THREE.PerspectiveCamera) {
     super();
-
-    this.terrain = new Terrain();
-    this.add(this.terrain);
   }
 
-  update(_dt: number) {
-    this.terrain.update(this.camera.position);
-  }
+  update(_dt: number) {}
 }
