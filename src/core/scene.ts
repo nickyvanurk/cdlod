bcdb7f0ba44d62bd4687329d444c1ecba73d4c54
Nicky van Urk <nickyvanurk@gmx.com>
2023-09-19 21:12:14 +0200

    Render instanced mesh grid

--- src/core/scene.ts
+++ src/core/scene.ts
@@ -1,8 +1,25 @@
 import * as THREE from 'three';
 
 export class Scene extends THREE.Scene {
-  constructor(private camera: THREE.PerspectiveCamera) {
+  constructor(_camera: THREE.PerspectiveCamera) {
     super();
+
+    const maxLod = 5;
+    const sectorSize = 64;
+    const maxScale = sectorSize * Math.pow(2, maxLod);
+    const geometry = new THREE.PlaneGeometry(maxScale, maxScale, sectorSize * 2, sectorSize * 2);
+    geometry.rotateX(-Math.PI / 2); // flip to xz plane
+    console.log(sectorSize * Math.pow(2, maxLod));
+
+    const material = new THREE.ShaderMaterial({
+      uniforms: {
+        sectorSize: { value: sectorSize },
+      },
+      wireframe: true,
+    });
+
+    const grid = new THREE.InstancedMesh(geometry, material, 1);
+    this.add(grid);
   }
 
   update(_dt: number) {}
