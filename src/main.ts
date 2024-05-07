793b02a13159d9bcf954c2066dd5513f206f91d2
Nick van Urk <nickyvanurk@gmx.com>
2024-05-07 22:46:37 +0200

    Remove terrain class

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,10 @@ import GUI from 'lil-gui';
 import * as THREE from 'three';
 import { MapControls } from 'three/examples/jsm/controls/MapControls';
 
+import gridFragmentShader from './grid.fs';
+import gridVertexShader from './grid.vs';
+import { Node as QuadTree } from './quad_tree';
 import { Stats } from './stats';
-import { Terrain } from './terrain';
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
@@ -15,32 +17,84 @@ camera.position.x = 1100;
 
 const controls = new MapControls(camera, renderer.domElement);
 
-const stats = new Stats(renderer);
-document.body.appendChild(stats.domElement);
+const scene = new THREE.Scene();
 
 const heightData = await loadHeightmap('./src/heightmap.raw');
 const texture = await loadTexture('./src/texture.png');
 
-const terrain = new Terrain(heightData, texture);
-const scene = new THREE.Scene();
-scene.add(terrain);
-
 const frustum = new THREE.Frustum();
 const mat4 = new THREE.Matrix4();
 
+const MAX_INSTANCES = 500;
+
+const tree = new QuadTree(0, 0, 2048, 0, heightData);
+
+const minLodDistance = 256;
+const lodLevels = 4;
+const lodRanges = [] as number[];
+for (let i = 0; i <= lodLevels; i++) {
+  lodRanges[i] = minLodDistance * Math.pow(2, 1 + lodLevels - i);
+}
+
+const colors = ['#33f55f', '#befc26', '#e6c12f', '#fc8e26', '#f23424'].map((c) => new THREE.Color(c));
+
+const sectorSize = 64;
+const geometry = new THREE.PlaneGeometry(1, 1, sectorSize, sectorSize);
+geometry.rotateX(-Math.PI / 2); // flip to xz plane
+
+const lodLevelAttribute = new THREE.InstancedBufferAttribute(new Float32Array(MAX_INSTANCES), 1, false, 1);
+geometry.setAttribute('lodLevel', lodLevelAttribute);
+
+const heightmap = new THREE.DataTexture(heightData, 4096, 4096, THREE.RedFormat, THREE.FloatType);
+heightmap.needsUpdate = true;
+
+const material = new THREE.ShaderMaterial({
+  uniforms: {
+    sectorSize: { value: sectorSize },
+    lodRanges: { value: lodRanges },
+    colors: { value: colors },
+    heightmap: { value: heightmap },
+    albedomap: { value: texture },
+    enableLodColors: { value: false },
+  },
+  vertexShader: gridVertexShader,
+  fragmentShader: gridFragmentShader,
+  wireframe: false,
+});
+
+const grid = new THREE.InstancedMesh(geometry, material, MAX_INSTANCES);
+grid.frustumCulled = false;
+grid.count = 1;
+scene.add(grid);
+
+const aabbHelpers = new THREE.Group();
+aabbHelpers.visible = false;
+scene.add(aabbHelpers);
+for (let i = 0; i < MAX_INSTANCES; i++) {
+  const geo = new THREE.BoxGeometry(1, 1, 1);
+  const edges = new THREE.EdgesGeometry(geo);
+  const aabb = new THREE.LineSegments(edges, new THREE.LineBasicMaterial({ color: 0xff0000 }));
+  aabb.material.depthTest = false;
+  aabb.visible = false;
+  aabbHelpers.add(aabb);
+}
+
+const stats = new Stats(renderer);
+document.body.appendChild(stats.domElement);
+
 const gui = new GUI();
 gui
-  .add(terrain.material, 'wireframe')
+  .add(material, 'wireframe')
   .name('Wireframe')
-  .onChange((visible: boolean) => (terrain.material.wireframe = visible));
+  .onChange((visible: boolean) => (material.wireframe = visible));
 gui
-  .add(terrain.material.uniforms.enableLodColors, 'value')
+  .add(material.uniforms.enableLodColors, 'value')
   .name('LOD Colors')
-  .onChange((enable: boolean) => (terrain.material.uniforms.enableLodColors.value = enable));
+  .onChange((enable: boolean) => (material.uniforms.enableLodColors.value = enable));
 gui
-  .add(terrain.aabbHelpers, 'visible')
+  .add(aabbHelpers, 'visible')
   .name('AABB')
-  .onChange((visible: boolean) => (terrain.aabbHelpers.visible = visible));
+  .onChange((visible: boolean) => (aabbHelpers.visible = visible));
 
 requestAnimationFrame(render);
 
@@ -50,7 +104,44 @@ function render() {
   const startTime = performance.now();
 
   frustum.setFromProjectionMatrix(mat4.multiplyMatrices(camera.projectionMatrix, camera.matrixWorldInverse));
-  terrain.update(camera.position, frustum);
+
+  const lodLevelAttribute = grid.geometry.getAttribute('lodLevel') as THREE.InstancedBufferAttribute;
+
+  if (aabbHelpers.visible) {
+    for (const helper of aabbHelpers.children) {
+      helper.visible = false;
+    }
+  }
+
+  const selectedNodes: { node: QuadTree; level: number }[] = [];
+  tree.selectNodes(camera.position, [...lodRanges].reverse(), 4, frustum, (node, level) => {
+    selectedNodes.push({ node, level });
+  });
+
+  for (const [idx, obj] of selectedNodes.entries()) {
+    grid.setMatrixAt(
+      idx,
+      new THREE.Matrix4().compose(
+        new THREE.Vector3(obj.node.x, 0, obj.node.y),
+        new THREE.Quaternion(),
+        new THREE.Vector3(obj.node.halfSize * 2, 1, obj.node.halfSize * 2)
+      )
+    );
+
+    lodLevelAttribute.set(Float32Array.from([obj.level]), idx);
+
+    if (aabbHelpers.visible) {
+      const yPos = (obj.node.min + obj.node.max) * 0.5;
+      const yScale = obj.node.max - obj.node.min;
+      aabbHelpers.children[idx].position.set(obj.node.x, yPos, obj.node.y);
+      aabbHelpers.children[idx].scale.set(obj.node.halfSize * 2, yScale, obj.node.halfSize * 2);
+      aabbHelpers.children[idx].visible = true;
+    }
+  }
+
+  lodLevelAttribute.needsUpdate = true;
+  grid.count = selectedNodes.length;
+  grid.instanceMatrix.needsUpdate = true;
 
   const endTime = performance.now() - startTime;
 
